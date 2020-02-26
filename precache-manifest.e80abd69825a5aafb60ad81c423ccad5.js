586257761d989c821a8fb012bda8c8542093e0d0
sugi <widjaja.s@gmail.com>
2020-02-26 00:08:55 -0800

    Updates

---
+++ precache-manifest.e80abd69825a5aafb60ad81c423ccad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6ed55a2689aa69c39b41949c329ba5",
+    "url": "/Refreshable-Braille-Display-Simulator/index.html"
+  },
+  {
+    "revision": "ff6744edec13b91862b7",
+    "url": "/Refreshable-Braille-Display-Simulator/static/css/main.21b47b73.chunk.css"
+  },
+  {
+    "revision": "b47bbf7d868a4bf8889f",
+    "url": "/Refreshable-Braille-Display-Simulator/static/js/2.5d8a9c44.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/Refreshable-Braille-Display-Simulator/static/js/2.5d8a9c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6744edec13b91862b7",
+    "url": "/Refreshable-Braille-Display-Simulator/static/js/main.53748285.chunk.js"
+  },
+  {
+    "revision": "37650fbda1c6ca09f840",
+    "url": "/Refreshable-Braille-Display-Simulator/static/js/runtime-main.2c0b15a0.js"
+  }
+]);
